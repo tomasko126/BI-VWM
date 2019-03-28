e5946862cc5fce9f9f4f45fd0f73b0455e7cc1d3
Jan Peřina <jan.perina@outlook.cz>
2019-03-28 21:50:43 +0100

    Delete list.py

--- list.py
+++
@@ -1,8 +0,0 @@
-import json
-with open('test.json', 'r') as f:
-    matrice = json.load(f)
-print(matrice)
-print(matrice[input()])
-with open('analyzed/_list.json', 'r') as t:
-    doc = json.load(t)
-    print(doc[input()])
