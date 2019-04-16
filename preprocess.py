1c2f9c10efba7ab4d7f3830499686edc7778648e
Tomáš Taro <tomas@getadblock.com>
2019-04-17 01:10:39 +0200

    Add json files created after lemmatization

--- preprocess.py
+++ preprocess.py
@@ -1,30 +1,42 @@
+import json
+import os
+import re
+
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from collections import Counter
-import re
-import json
-import os
 
 
 def lemmatize():
     counter = 0
     articles = dict()
+
     for item in os.listdir('plain_articles'):
         counter = counter + 1
         articles.update({int(counter): item})
+
     with open('analyzed/_list.json', 'w') as listF:
         json.dump(articles, listF, ensure_ascii=False)
 
     lemmatizer = WordNetLemmatizer()
+
     for file in os.listdir('plain_articles'):
         with open('plain_articles/'+file, 'r') as reader:
-            text = reader.read().lower()
-        text = re.sub(r'[^a-z ]', '', text)
-        tokens = word_tokenize(text)
-        words = list()
-        [words.append(lemmatizer.lemmatize(x)) for x in tokens if x not in stopwords.words('english')]
-        result = Counter(words)
+            try:
+                text = reader.read().lower()
 
-        with open('analyzed/'+file+".json", 'w') as writer:
-            json.dump(result, writer)
+                text = re.sub(r'[^a-z ]', '', text)
+                tokens = word_tokenize(text)
+
+                words = list()
+                [words.append(lemmatizer.lemmatize(x)) for x in tokens if x not in stopwords.words('english')]
+                result = Counter(words)
+
+                with open('analyzed/' + file + ".json", 'w') as writer:
+                    json.dump(result, writer)
+            except UnicodeDecodeError:
+                if file == '.DS_Store':
+                    continue
+                else:
+                    raise
